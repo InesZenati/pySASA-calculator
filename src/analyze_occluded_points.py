b751bd46c72d063c4208621ba55076e7d7997594
InesZenati <ineshanna31@gmail.com>
2026-09-08 18:01:55 +0200

    feat: add a function to compute the surface off all atoms

--- src/analyze_occluded_points.py
+++ src/analyze_occluded_points.py
@@ -11,27 +11,34 @@ from tqdm import tqdm
 
 from parse_pdb import load_radius_json, parse_pdb
 
-
-def compute_atom_surface(atom):
+def compute_atom_surface_infos(atom):
     """
     Compute point counts and surface areas (total, occluded, accessible) of one atom.
     
-    
     """
-    number_points = len(atom.sphere.pointlist)
+    atom_infos  = {}
+    number_of_points = len(atom.sphere.pointlist)
     number_of_occluded_atoms = sum(atom.sphere.occluded_points) 
-    n_accessible = number_points - number_of_occluded_atoms
+    number_of_accessible = number_of_points - number_of_occluded_atoms
 
     total_surface = 4 * math.pi * atom.sphere.radius ** 2
-    occluded_surface = (total_surface / number_points) * number_of_occluded_atoms
+    occluded_surface = (total_surface / number_of_points) * number_of_occluded_atoms
     accessible_surface = total_surface - occluded_surface
 
-    return {
-        "n_points": number_points,
-        "n_occluded": number_of_occluded_atoms,
-        "n_accessible": n_accessible,
+    atom_infos = {
+        "number_of_points": number_of_points,
+        "number_of_occluded_atoms": number_of_occluded_atoms,
+        "number_of_accessible": number_of_accessible,
         "total_surface": total_surface,
         "occluded_surface": occluded_surface,
         "accessible_surface": accessible_surface,
     }
+    
+    return atom_infos
+
+def compute_all_atoms_surface_infos(atomlist):
+    """Sum point counts and surfaces over a list of atoms."""
+    for atom in atomlist:
+        atom_result = compute_atom_surface_infos(atom)
+    return atom_result
 
