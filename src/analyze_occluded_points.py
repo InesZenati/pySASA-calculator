c17f43bd56ae3f38cdead5a7188a239cdc91147d
InesZenati <ineshanna31@gmail.com>
2026-09-09 10:22:14 +0200

    fix : correct import and complete docstring

--- src/analyze_occluded_points.py
+++ src/analyze_occluded_points.py
@@ -3,7 +3,7 @@ import math
 import os
 import sys
 import time
-import Path
+from pathlib import Path
 from datetime import UTC, datetime
 
 import click
@@ -16,6 +16,15 @@ def compute_atom_surface(atom_object):
     """
     Compute point counts and surface areas (total, occluded, accessible) of one atom.
     
+    Parameters
+    ----------
+    atom_object : Atom
+        An instance of the Atom class containing information about the atom and its sphere.
+        
+    Returns
+    -------
+    dict : 
+        A dictionary containing the number of points, number of occluded atoms, number of accessible atoms,
     """
     atom_infos  = {}
     number_of_points = len(atom_object.sphere.pointlist)
@@ -37,9 +46,22 @@ def compute_atom_surface(atom_object):
     
     return atom_infos
 
-def compute_residu_surface(residue):
+def compute_residu_surface(residue_object):
+    """ 
+    Compute the surface area of a residue by summing the surface areas of its atoms.
+    
+    Parameters
+    ----------
+    residue_object : Residues
+        An instance of the Residues class containing information about the residue and its atoms.
+    
+    Return
+    -------
+    dict : 
+        A dictionary containing the total number of points, number of occluded atoms, number of accessible atoms,
+    """
     result = {}
-    atom_list = residue.atomlist
+    atom_list = residue_object.atomlist
     for atom in atom_list:
         atom_result = compute_atom_surface(atom)
         for key, value in atom_result.items():
@@ -47,11 +69,27 @@ def compute_residu_surface(residue):
     return result
 
 
-def compute_chain_surface(residuelist, chain_id):
+def compute_chain_surface(protein, chain_id):
+    """
+    Compute the surface area of a chain by summing the surface areas of its residues.
+    
+    Parameters
+    ----------
+    protein : Protein
+        An instance of the Protein class containing information about the protein and its residues.
+    
+    chain_id : str
+        The identifier of the chain for which to compute the surface area.
+    
+    Return
+    ------
+    dict :  
+        A dictionary containing the total number of points, number of occluded atoms, number of accessible atoms,
+    """
 
     chain_atoms = []
     result = {}
-    for residue in residuelist:
+    for residue in protein.residuelist:
         if residue.chain_id == chain_id:
             chain_atoms.extend(residue.atomlist)
     for atom in chain_atoms:
@@ -61,6 +99,19 @@ def compute_chain_surface(residuelist, chain_id):
     return result
     
 def get_chain_ids(residuelist):
+    """
+    Extract unique chain identifiers from a list of residues.
+    
+    Parameters
+    ----------
+    residuelist : list
+        A list of Residue objects from which to extract chain identifiers.
+    
+    Return
+    ------
+    set :
+        A sorted list of unique chain identifiers present in the provided list of residues.
+    """
     chain_ids = set()
     for residue in residuelist:
         chain_ids.add(residue.chain_id)
@@ -69,9 +120,8 @@ def get_chain_ids(residuelist):
 
 @click.command()
 @click.option("--pdb-name", 
-              type=click.Path(file_okay=True, path_type=Path),
-              help="Name of the protein (PDB id).")
-                          
+              type=str,
+              help="Name of the protein (PDB id).")                      
 @click.option("--pdb-file", 
               type=click.Path(file_okay=True, path_type=Path), 
               help="Path to the PDB file.")
@@ -80,7 +130,7 @@ def get_chain_ids(residuelist):
               help="Path to the JSON file with van der Waals radius tables.")
 def analyze_protein_sasa(pdb_name, pdb_file, radius_json):
     radius_table= load_radius_json(radius_json)
-    protein = parse_pdb(radius_table, pdb_name, pdb_file)
+    protein = parse_pdb(pdb_file, pdb_name, radius_table)
 
     all_atoms = protein.get_all_atom()
     logger.info(f"Total number of atoms: {len(all_atoms)}")
