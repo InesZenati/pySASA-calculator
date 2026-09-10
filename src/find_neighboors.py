1baecaf5382edb4058f691acb7a8b838e617f089
InesZenati <ineshanna31@gmail.com>
2026-09-10 12:04:18 +0200

    fix : change the cutoff

--- src/find_neighboors.py
+++ src/find_neighboors.py
@@ -18,6 +18,21 @@ def build_neighbor_search(all_atoms):
     """
     return NeighborSearch(all_atoms)
 
+def get_max_radius(all_atoms):
+    """
+    Get the largest sphere radius found among all atoms of the protein.
+
+    Parameters
+    ----------
+    all_atoms : list
+        A list of all Atom objects in the protein.
+
+    Return
+    ------
+    float :
+        The largest sphere radius, in Angstroms.
+    """
+    return max(atom.sphere.radius for atom in all_atoms)
 
 def get_max_cutoff(all_atoms):
     """
@@ -42,9 +57,33 @@ def get_max_cutoff(all_atoms):
     return 2 * max_radius
 
 
-def get_atom_neighbors(atom, neighbor_search, cutoff):
+def get_atom_cutoff(atom, max_radius):
     """
-    Get the atoms within cutoff distance from an atom.
+    Compute the cutoff distance for one atom's neighbor search.
+
+    Two atoms i and j can only occlude each other if the distance between
+    them is smaller than the sum of their sphere radius (Ri + Rj). Since we
+    do not know Rj in advance, we use the largest possible radius found in
+    the protein as an upper bound for Rj.
+
+    Parameters
+    ----------
+    atom : Atom
+        The atom object for which the cutoff is computed.
+    max_radius : float
+        The largest sphere radius found among all atoms of the protein.
+
+    Return
+    ------
+    float :
+        The cutoff distance for this atom's neighbor search, in Angstroms.
+    """
+    return atom.sphere.radius + max_radius
+
+
+def get_atom_neighbors(atom, neighbor_search, max_radius):
+    """
+    Get the atoms within occlusion distance from an atom.
 
     Parameters
     ----------
@@ -52,17 +91,18 @@ def get_atom_neighbors(atom, neighbor_search, cutoff):
         The atom object around which to search neighbors.
     neighbor_search : NeighborSearch
         A NeighborSearch instance built from all the atoms of the protein.
-    cutoff : float
-        The maximum distance (in Angstroms) to consider an atom a neighbor.
+    max_radius : float
+        The largest sphere radius found among all atoms of the protein.
 
     Return
     ------
     list :
-        A list of Atom objects within cutoff distance from atom, excluding
-        the atom itself.
+        A list of Atom objects that could occlude atom, excluding the atom
+        itself.
     """
+    atom_cutoff = get_atom_cutoff(atom, max_radius)
+    neighbor_atoms = neighbor_search.search(atom.get_coord(), atom_cutoff)
     neighbor_atoms_list = []
-    neighbor_atoms = neighbor_search.search(atom.get_coord(), cutoff)
     for neighbor_atom in neighbor_atoms:
         if neighbor_atom is not atom:
             neighbor_atoms_list.append(neighbor_atom)
